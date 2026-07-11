c4208c33faeb17c280cfcb50f614a2e8755dae96
AditiWagh1 <mikadu.wagh@gmail.com>
2026-07-11 14:18:44 +0530

    1211-queries-quality-and-percentage

---
+++ 1211-queries-quality-and-percentage/1211-queries-quality-and-percentage.sql
@@ -0,0 +1,2 @@
+# Write your MySQL query statement below
+select query_name,ROUND(AVG(rating/position),2) as quality,ROUND(SUM(CASE WHEN rating<3 THEN 1 ELSE 0 END)*100/COUNT(*),2) as poor_query_percentage from Queries where query_name IS NOT NULL GROUP BY query_name;
