84063d66f78b65aa33328c3a281ec0fa945c6439
AditiWagh1 <mikadu.wagh@gmail.com>
2026-07-11 22:53:33 +0530

    1174-immediate-food-delivery-ii

---
+++ 1174-immediate-food-delivery-ii/1174-immediate-food-delivery-ii.sql
@@ -0,0 +1,9 @@
+# Write your MySQL query statement below
+select
+    ROUND(SUM(IF(order_date=customer_pref_delivery_date,1,0))*100/COUNT(*),2) as immediate_percentage
+from Delivery
+where (customer_id,order_date) IN(
+    SELECT customer_id,MIN(order_date)
+    FROM delivery
+    GROUP BY customer_id 
+);
